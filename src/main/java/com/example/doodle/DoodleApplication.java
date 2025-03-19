b590d21f3cbb49071f00c648f668ecf25f0135fd
Dain Jeong <dainjeong1004@gmail.com>
2025-03-19 11:37:42 +0900

    [Feat] set spring initializr

---
+++ src/main/java/com/example/doodle/DoodleApplication.java
@@ -0,0 +1,13 @@
+package com.example.doodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodleApplication.class, args);
+	}
+
+}
